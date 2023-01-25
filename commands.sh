f7c89e3b55cf98d659d8671efb70a69240ab1e72
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-25 12:39:49 +0530

    Commands
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ commands.sh
@@ -0,0 +1,30 @@
+mkdir dirName # Create dir
+
+ls # list contents
+ls -al
+ls -alt # list all the files in long format sorted by time.
+
+su # Enter sudo mode
+
+touch fileName # Create file 
+
+cat > file1.txt # Enter text via terminal || Create file if it does not exit.
+cat >> file1.txt # Appends data in file
+
+command > newFile.txt # Puts the output of command in file.
+
+rm fileName # remove file 
+
+# Use tab to autocomplete.
+
+rm *.txt # asterix is the wildcard character. Replace * with 
+# all sorts of possible combination
+
+cp sourceFile destinationPath
+
+mv sourceFile destinationPath
+
+rm -d dirName # Need to add -d to remove empty dir.
+
+mv file1.txt file2.txt # rename file1 to file2.
+

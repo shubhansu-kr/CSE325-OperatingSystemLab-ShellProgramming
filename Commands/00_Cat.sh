f1bb7c1cea8b42fec69171c52dac3faf01f58665
Shubhansu kumar <shubhansu2021@gmail.com>
2023-01-25 12:56:09 +0530

    Experiment 2
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Commands/00_Cat.sh
+++ Commands/00_Cat.sh
@@ -1,3 +1,5 @@
+# !/bin/bash
+
 # Cat Command
 
 cat text1.txt
---
+++ Experiments/01_Experiment2.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+
+# Extention of script - .sh
+touch 00_HelloWorld.sh 
+
+# Vi is the editor 
+vi 00_HelloWorld.sh  
+
+# once in vi editor 
+# press i : insert mode 
+# press ctrl+c : freeze 
+# press shift+: opens saving 
+# In : mode press wq 
+
+# Shell program 
+
+# !/bin/bash : Tells the type of shell
+# Add series of commands 
+
+`
+# !/bin/bash
+echo "Hello World"
+`

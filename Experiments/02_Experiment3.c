1093329d0210a5b904035f4d02a948755f508da2
Shubhansu kumar <shubhansu2021@gmail.com>
2023-02-22 12:45:44 +0530

    Exp3
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Experiments/02_Experiment3.c
+++ Experiments/02_Experiment3.c
@@ -20,6 +20,29 @@ void practice()
     printf("Hello World");
 }
 
+void fileToConsole() {
+    FILE* ptr;
+	char ch;
+
+	// Opening file in reading mode
+	ptr = fopen("Sample.txt", "r");
+
+	if (NULL == ptr) {
+		printf("file can't be opened \n");
+	}
+
+	printf("content of this file are \n");
+
+	do {
+		ch = fgetc(ptr);
+		printf("%c", ch);
+
+	} while (ch != EOF);
+
+	fclose(ptr);
+	return 0;
+}
+
 void printWithoutUsingCharArray() {
     // Exercise Q3
     char ch;

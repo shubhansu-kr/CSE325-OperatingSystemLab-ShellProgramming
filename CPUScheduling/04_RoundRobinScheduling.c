72aa746db7e6412fc7cd72ef708736a09658c351
Shubhansu kumar <shubhansu2021@gmail.com>
2023-03-30 22:51:50 +0530

    Round Robin
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- CPUScheduling/04_RoundRobinScheduling.c
+++ CPUScheduling/04_RoundRobinScheduling.c
@@ -1,79 +1,83 @@
 #include <stdio.h>
-
-#define MAX_PROCESSES 100
-
 struct Process {
-    int pid;
-    int arrival_time;
-    int burst_time;
-    int remaining_time;
-    int waiting_time;
-    int turnaround_time;
+    int pId;
+    int arrivalTime;
+    int burstTime;
 };
 
 void round_robin(struct Process p[], int n, int quantum) {
-    int remaining_time[MAX_PROCESSES];
-    int completed[MAX_PROCESSES] = {0};
-    int total_waiting_time = 0;
-    int total_turnaround_time = 0;
-    int current_time = 0;
-    int completed_count = 0;
+    int remainingTime[n], WT[n], TAT[n], CT[n];
+    int totalWT = 0, totalTAT = 0, currentTime = 0, completedCount = 0;
 
     for (int i = 0; i < n; i++) {
-        remaining_time[i] = p[i].burst_time;
+        remainingTime[i] = p[i].burstTime;
     }
 
-    while (completed_count < n) {
+    while (completedCount < n) {
+
         for (int i = 0; i < n; i++) {
-            if (p[i].arrival_time <= current_time && remaining_time[i] > 0) {
-                if (remaining_time[i] <= quantum) {
-                    current_time += remaining_time[i];
-                    p[i].turnaround_time = current_time - p[i].arrival_time;
-                    p[i].waiting_time = p[i].turnaround_time - p[i].burst_time;
-                    total_waiting_time += p[i].waiting_time;
-                    total_turnaround_time += p[i].turnaround_time;
-                    remaining_time[i] = 0;
-                    completed_count++;
+            if (p[i].arrivalTime <= currentTime && remainingTime[i] > 0) {
+                if (remainingTime[i] <= quantum) {
+                    currentTime += remainingTime[i];
+
+                    CT[i] = currentTime;
+
+                    TAT[i] = CT[i] - p[i].arrivalTime;
+                    totalTAT += TAT[i];
+
+                    WT[i] = TAT[i] - p[i].burstTime;
+                    totalWT += WT[i];
+
+                    remainingTime[i] = 0;
+                    completedCount++;
+
                 } else {
-                    current_time += quantum;
-                    remaining_time[i] -= quantum;
+                    currentTime += quantum;
+                    remainingTime[i] -= quantum;
                 }
             }
         }
     }
 
-    printf("PID\tAT\tBT\tWT\tTAT\n");
+    printf("PID\tAT\tBT\tCT\tTAT\tWT\n");
 
-    for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t%d\t%d\t%d\n", p[i].pid, p[i].arrival_time, p[i].burst_time, p[i].waiting_time, p[i].turnaround_time);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i].pId, p[i].arrivalTime, p[i].burstTime, CT[i], TAT[i], WT[i]);
     }
 
-    float avg_waiting_time = (float)total_waiting_time / n;
-    float avg_turnaround_time = (float)total_turnaround_time / n;
+    float averageWT = (float)totalWT / n;
+    float averageTAT = (float)totalTAT / n;
 
-    printf("Average waiting time: %.2f\n", avg_waiting_time);
-    printf("Average turnaround time: %.2f\n", avg_turnaround_time);
+    printf("\nAverage waiting time: %.2f\n", averageWT);
+    printf("Average turnaround time: %.2f\n", averageTAT);
 }
 
 int main() {
     int n, quantum;
-    struct Process p[MAX_PROCESSES];
 
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    printf("Enter the arrival time and burst time of each process:\n");
+    struct Process pcb[n];
+
+    printf("Enter the arrival time and burst time of each process:\n\n");
     for (int i = 0; i < n; i++) {
         printf("Process %d: ", i + 1);
-        scanf("%d %d", &p[i].arrival_time, &p[i].burst_time);
-        p[i].pid = i + 1;
-        p[i].remaining_time = p[i].burst_time;
+
+        printf("\n\nArrivalTime: ");
+        scanf("%d", &pcb[i].arrivalTime);
+        printf("BurstTime: ");
+        scanf("%d", &pcb[i].burstTime);
+        printf("\n");
+
+        pcb[i].pId = i + 1;
     }
 
     printf("Enter the time quantum: ");
-    scanf("%d", &quantum);
+    scanf("%d", &quantum);    
 
-    round_robin(p, n, quantum);
+    round_robin(pcb, n, quantum);
 
     return 0;
 }

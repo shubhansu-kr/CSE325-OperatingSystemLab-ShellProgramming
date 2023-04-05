8864d0387c9343ca6005036c4e40e24a79f12414
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-05 12:08:51 +0530

    Print Fork Cases
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- Experiments/04_Experiment5.c
+++ Experiments/04_Experiment5.c
@@ -5,6 +5,31 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+void printFork2() {
+
+    pid_t childPid = fork();
+    pid_t childPid1 = fork();
+
+    printf("printf\n"); // printed only 4 times: 1 from parent. 3 from child.
+
+}
+
+void printFork1() {
+
+    printf("printf\n"); // printed only 1 times with '\n' : 1 from parent. 0 from child.
+
+    pid_t childPid = fork();
+    pid_t childPid1 = fork();
+}
+
+void printFork() {
+
+    printf("printf"); // printed 4 times since print statement is without \n
+
+    pid_t childPid = fork();
+    pid_t childPid1 = fork();
+}
+
 void createChild() {
     // Fork() system call is mechanism in unix based os that 
     // allows to create a process to create a new child 

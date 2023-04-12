7a3a5dc5a0a9289e65da058d75815fd9840896bd
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-12 05:38:12 +0530

    Ex3 Exp 5
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ Fork/04_Exercise2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid1, pid2;
+
+    // create process P1
+    pid1 = fork();
+
+    if (pid1 < 0)
+    {
+        printf("Failed to create process P1.\n");
+        return 1;
+    }
+    else if (pid1 == 0)
+    {
+        // P2 process
+        printf("P2 process created with pid: %d, parent pid: %d\n", getpid(), getppid());
+        pid2 = fork();
+
+        if (pid2 < 0)
+        {
+            printf("Failed to create process P2.\n");
+            return 1;
+        }
+        else if (pid2 == 0)
+        {
+            // P3 process
+            printf("P3 process created with pid: %d, parent pid: %d\n", getpid(), getppid());
+            return 0;
+        }
+    }
+    else
+    {
+        // P1 process
+        printf("P1 process created with pid: %d\n", getpid());
+    }
+
+    return 0;
+}

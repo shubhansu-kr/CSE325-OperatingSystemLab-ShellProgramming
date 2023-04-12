bc4024e25254e28980188b551ac04fa83da7b2ef
Shubhansu kumar <shubhansu2021@gmail.com>
2023-04-12 05:35:13 +0530

    Exp5 Exercise 1
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ Fork/03_Exercise1.c
@@ -0,0 +1,35 @@
+//1
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid;
+
+    int n; 
+    printf("Enter no. of fork calls: ");
+    scanf("%d", &n);
+
+    int count = 0;
+
+    for (int i = 0; i < n; ++i) {
+        pid = fork();
+
+        if (pid < 0) {
+            printf("Fork failed.\n");
+            return 1;
+        } else if (pid == 0) {
+            // child process
+            printf("Child %d with parent %d.\n", getpid(), getppid());
+        } else {
+            // parent process
+            count++;
+        }
+    }
+
+    printf("Total number of child processes created: %d\n", (1 << n) - 1);
+
+    return 0;
+}
